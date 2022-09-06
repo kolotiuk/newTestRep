77e44a84291cabcdef7bf67989471c5c258e41bc
EgorRybin <antoniopareiro@gmail.com>
2022-09-06 20:07:14 +0300

    update reg

--- src/js/services/regisrtetion.js
+++ src/js/services/regisrtetion.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 const provider = new GoogleAuthProvider();
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyC-rMEvEUJ6DbhIaJz-m19MpHLMzbQ1hi4',
   authDomain: 'auth-9127f.firebaseapp.com',
@@ -16,21 +11,19 @@ const firebaseConfig = {
   appId: '1:1077758083281:web:0634a927ba2da6007f85f0',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const provider = new app.auth.GoogleAuthProvider();
 
-const button = document.querySelector('#google');
-// button.addEventListener('click', onAuthGoogle);
+const button = document.querySelector('.header__singup-link');
+const ul = document.querySelector('.header__list');
+
+button.addEventListener('click', onAuthGoogle);
 
 function onAuthGoogle() {
   const auth = getAuth();
   signInWithPopup(auth, provider)
     .then(result => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
-      // The signed-in user info.
       const user = result.user;
       console.log(user);
 
@@ -38,26 +31,16 @@ function onAuthGoogle() {
       if (user.email) {
         button.classList.add('is-hidden');
         console.log(`Singed in as ${user.displayName}`);
+        ul.insertAdjacentHTML(
+          'beforeend',
+          `<li class="header__ref header__contact-link">Singed in as ${user.displayName}</li>`
+        );
       }
-
-      // ...
     })
     .catch(error => {
-      // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
-      // The email of the user's account used.
       const email = error.customData.email;
-      // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
     });
-}
-
-function setToLocalStorage() {
-  const user = {
-    displayName: user.displayName,
-    name: user.name,
-  };
-  localStorage.setItem('user', JSON.stringify(user));
 }
