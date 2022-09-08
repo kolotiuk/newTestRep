865b33b8b4fd7e5fd1491cbbfe94c630d91861d1
Ihor <93486946+kolotiuk@users.noreply.github.com>
2022-09-08 11:11:17 +0300

    Update render-my-library.js

--- src/js/services/render-my-library.js
+++ src/js/services/render-my-library.js
@@ -3,7 +3,7 @@ import { ref } from './switch-home-library';
 function renderEmptyLibrary() {
   return `<div class='container content__library'>
           <div class='content__library-container'>
-          <img  class='content__library__img' src='https://assets.bigcartel.com/product_images/228233459/PikachuMeme.png?auto=format&fit=max&w=1500' loading='lazy' alt='pikachu' width="400" height="400"/>
+          <img  class='content__library__img' src='https://i.imgur.com/3afQVRr.png' loading='lazy' alt='pikachu' width="350" height="350"/>
           </div>
           <div class='content__library-ref'>
           <h2 class='content__library-title'>Ooops... nothing was selected!</h2>
@@ -17,11 +17,37 @@ const removeSelector = document.querySelector('.movie-cards__list');
 
 ref.libraryBtn.addEventListener('click', onHandleBtnClick);
 
+import renderMarkupMovieCards from '../templates/movie-card';
+import infiniteObserver from './infinityScroll';
+const getWathed = localStorage.getItem('watched');
+const wathedPArse = JSON.parse(getWathed) ?? [];
+// const updateMovies = [...wathedPArse, ]
+
 function onHandleBtnClick() {
   if (ref.libraryBtn.classList.contains('current')) {
-    ref.gallery.innerHTML = '';
-    removeSelector.classList.remove('movie-cards__list');
+    // ref.gallery.innerHTML = '';
+    // infiniteObserver.disconnect();
+    console.log('~ wathedPArse', wathedPArse);
+
+    renderMarkupMovieCards(wathedPArse, mylibrary = true);
+    // console.log("~ markup", markup)
+    // ref.gallery.innerHTML = markup;
+    // console.log('~ wathedPArse', wathedPArse);
+
+    // wathedPArse.forEach(el => {
+    //   console.log("~ el", el)
+    //   const objVal = Object.values(el);
+    //   console.log("~ objVal", objVal)
+    //   // ref.gallery.insertAdjacentHTML('beforebegin', element);
+    //   // ref.gallery.innerHTML = renderMarkupMovieCards(el);
+    // });
+    // ref.gallery.innerHTML = '';
+    // removeSelector.classList.remove('movie-cards__list');
   }
 
-  ref.gallery.innerHTML = renderEmptyLibrary();
+  // ref.gallery.innerHTML = renderEmptyLibrary();
 }
+
+// wathedPArse.forEach(element => {
+//   console.log(element);
+// });
