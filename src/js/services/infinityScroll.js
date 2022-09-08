b3234039cda5bc181bf62a2681ebb3cc265b07e5
Ihor <93486946+kolotiuk@users.noreply.github.com>
2022-09-08 11:11:49 +0300

    Update infinityScroll.js

--- src/js/services/infinityScroll.js
+++ src/js/services/infinityScroll.js
@@ -1,27 +1,27 @@
-import TheMovieAPI from './theMovieAPI';
-import { theMovieAPI } from './movieSearch';
-import renderMarkupMovieCards from '../templates/movie-card';
+// import TheMovieAPI from './theMovieAPI';
+// import { theMovieAPI } from './movieSearch';
+// import renderMarkupMovieCards from '../templates/movie-card';
 
-const infiniteObserver = new IntersectionObserver(
-  ([entry], observer) => {
-    if (entry.isIntersecting) {
-      observer.unobserve(entry.target);
-      loadMoreContent();
-    }
-  },
-  {
-    threshold: 0.7,
-  }
-);
-export default infiniteObserver;
+// const infiniteObserver = new IntersectionObserver(
+//   ([entry], observer) => {
+//     if (entry.isIntersecting) {
+//       observer.unobserve(entry.target);
+//       loadMoreContent();
+//     }
+//   },
+//   {
+//     threshold: 0.7,
+//   }
+// );
+// export default infiniteObserver;
 
-async function loadMoreContent() {
-  theMovieAPI.incrementPage();
+// async function loadMoreContent() {
+//   theMovieAPI.incrementPage();
 
-  try {
-    const { results } = theMovieAPI.query
-      ? await theMovieAPI.fetchSearchMovies()
-      : await theMovieAPI.fetchMovies();
-    renderMarkupMovieCards(results);
-  } catch (error) {}
-}
+//   try {
+//     const { results } = theMovieAPI.query
+//       ? await theMovieAPI.fetchSearchMovies()
+//       : await theMovieAPI.fetchMovies();
+//     renderMarkupMovieCards(results);
+//   } catch (error) {}
+// }
